5547e5cb02d442ea9794950a4d71a2cbbaf540f1
dagoberto <berto35572@gmail.com>
2021-07-16 10:26:44 -0600

    setting up ScoreboardFragment and layout

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoreboardFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoreboardFragment.java
@@ -11,24 +11,30 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.FragmentScoreboardBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.DashboardViewModel;
+import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import org.jetbrains.annotations.NotNull;
 
 public class ScoreboardFragment extends Fragment {
 
-  private DashboardViewModel dashboardViewModel;
+  private GameViewModel viewModel;
+  private FragmentScoreboardBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    dashboardViewModel =
-        new ViewModelProvider(this).get(DashboardViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_scoreboard, container, false);
-    final TextView textView = root.findViewById(R.id.text_dashboard);
-    dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
-    });
-    return root;
+    super.onCreateView(inflater, container, savedInstanceState);
+    binding = FragmentScoreboardBinding.inflate(inflater, container, false);
+    //TODO Attach listeners to sliders
+    return binding.getRoot();
+
   }
+
+  @Override
+  public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel= new ViewModelProvider(getActivity()).get(GameViewModel.class);
+    //TODO add observers.
+  }
+
 }
